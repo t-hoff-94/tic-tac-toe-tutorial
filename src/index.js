8fe43321d35dc8bb1948697eba8d5308ee595a62
Tanner <tannerjhoffman@gmail.com>
2018-11-12 10:46:13 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ function Square(props) {
 
 
 class Board extends React.Component {
+
   renderSquare(i) {
     return (
     <Square
@@ -53,12 +54,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -70,15 +72,32 @@ class Game extends React.Component {
       history: history.concat([{
        squares: squares,
      }]),
+      stepNumber: history.length,
       xIsNext: this.state.xIsNext === true ? false : true,
     });
   }
 
+  jumpTo (step) {
+    console.log(step)
+    this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-    console.log(history)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move # ${move}` : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={()=> this.jumpTo(move)}> {desc} </button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -96,7 +115,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
